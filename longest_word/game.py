28a7fe77d49cfa4198a975a63d866dc954401b65
Antoine Matta <antoine.matta@hotmail.com>
2023-10-11 14:19:39 +0000

    implementing black

--- longest_word/game.py
+++ longest_word/game.py
@@ -4,33 +4,34 @@ A python implementation of the longest word game
 import random
 import requests
 
+
 class Game:
     """
     Game classe with multiple methods.
     init to generate the grid
     is_valid to test validity of word
     """
+
     def __init__(self) -> list:
         """Attribute a random grid to size 9"""
-        self.grid = [chr(random.randint(ord('A'),ord('Z'))) for _ in range(9)]
+        self.grid = [chr(random.randint(ord("A"), ord("Z"))) for _ in range(9)]
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
         if not word:
             return False
-        test_grid=self.grid.copy()
+        test_grid = self.grid.copy()
         for letter in word:
             if letter not in test_grid:
                 return False
             test_grid.remove(letter)
-        dict_url="https://wagon-dictionary.herokuapp.com/"
-        check_dict=requests.get(f"{dict_url}{word}").json()
+        dict_url = "https://wagon-dictionary.herokuapp.com/"
+        check_dict = requests.get(f"{dict_url}{word}").json()
         return check_dict["found"]
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     game = Game()
-    print(game.grid) # --> OQUWRBAZE
+    print(game.grid)  # --> OQUWRBAZE
     my_word = "BAROQUE"
-    game.is_valid(my_word) # --> True
+    game.is_valid(my_word)  # --> True
--- longest_word/wsgi.py
+++ longest_word/wsgi.py
@@ -7,23 +7,31 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-SESSION_TYPE = 'filesystem'
-SESSION_FILE_DIR = '/tmp'
+SESSION_TYPE = "filesystem"
+SESSION_FILE_DIR = "/tmp"
 app.config.from_object(__name__)
 Session(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     game = Game()
-    session['score']=session.get('score',0)
-    return render_template('home.html', grid=game.grid)
+    session["score"] = session.get("score", 0)
+    return render_template("home.html", grid=game.grid)
 
-@app.route('/check', methods=["POST"])
+
+@app.route("/check", methods=["POST"])
 def check():
     game = Game()
-    game.grid = list(request.form['grid'])
-    word = request.form['word']
+    game.grid = list(request.form["grid"])
+    word = request.form["word"]
     is_valid = game.is_valid(word)
     if is_valid:
-        session['score']+=len(word)
-    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word, score=session['score'])
+        session["score"] += len(word)
+    return render_template(
+        "check.html",
+        is_valid=is_valid,
+        grid=game.grid,
+        word=word,
+        score=session["score"],
+    )
