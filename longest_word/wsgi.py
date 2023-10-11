3034d61e0b3c2627ced4de7991e7214cca6b0921
Antoine Matta <antoine.matta@hotmail.com>
2023-10-11 13:19:58 +0000

    implemented score

--- longest_word/wsgi.py
+++ longest_word/wsgi.py
@@ -1,14 +1,20 @@
 # pylint: disable=missing-docstring
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from longest_word.game import Game
+from flask_session import Session
 
 app = Flask(__name__)
+
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def home():
     game = Game()
+    session['score']=session.get('score',0)
     return render_template('home.html', grid=game.grid)
 
 @app.route('/check', methods=["POST"])
@@ -17,4 +23,6 @@ def check():
     game.grid = list(request.form['grid'])
     word = request.form['word']
     is_valid = game.is_valid(word)
-    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+    if is_valid:
+        session['score']+=len(word)
+    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word, score=session['score'])
